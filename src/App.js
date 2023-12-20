fb590db25871054870eaec23c5d8610147f7c089
nurkeldii <beibir03nurkeldi@gmail.com>
2023-12-20 21:33:54 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, {useEffect, useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavourite from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
+const App =()=>{
+    const [movies, setMovies] = useState([]);
+    const [favourites, setFavourites] = useState([])
+    const [searchValue, setSearchValue] = useState('');
+
+    const getMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=1914b232`
+        const response = await fetch(url);
+        const responseJson = await response.json();
+
+        if (responseJson.Search) {
+            setMovies(responseJson.Search);
+        }
+    }
+    useEffect(() => {
+        getMovieRequest(searchValue);
+    }, [searchValue]);
+
+
+    useEffect(()=> {
+        const movieFavourites= JSON.parse(
+            localStorage.getItem('react-movie-app-favourites')
+        );
+        if (movieFavourites) {
+            setFavourites(movieFavourites);
+        }
+    }, [])
+
+    const saveToLocalStorage = (items) => {
+        localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+    }
+
+    const AddFavouriteMovie =(movie)=> {
+        const newFavouriteList=[...favourites, movie];
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+    }
+    const removeFavouriteMovie =(movie) => {
+        const newFavouriteList = favourites.filter(
+            (favourite)=> favourite.imdbID !== movie.imdbID
+        );
+
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+    }
+
+    return (
+        <div className='container-fluid movie-app'>
+            <div className='row d-flex align-items-lg-start mt-4 mb-4'>
+                <MovieListHeading heading='Фильмы'/>
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
+           <div className='row'>
+               <MovieList
+                   movies = {movies}
+                   handleFavouritesClick={AddFavouriteMovie}
+                   favouriteComponent = {AddFavourite}
+               />
+           </div>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Избранное'
+                />
+                <div className='row'>
+                    <MovieList
+                        movies={favourites}
+                        handleFavouritesClick={removeFavouriteMovie}
+                        favouriteComponent={RemoveFavourites}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
